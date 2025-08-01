4e8b30d847054d8cc97b20f0209d847be22e8fb5
Clickadelic <toby.hopp@gmail.com>
2025-08-01 17:01:52 +0200

    Fix case sensitivity issues with App.tsx references

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import path from 'path';
 export default defineConfig({
     plugins: [
         laravel({
-            input: 'resources/js/app.tsx',
+            input: 'resources/js/App.tsx',
             refresh: true,
         }),
         react(),

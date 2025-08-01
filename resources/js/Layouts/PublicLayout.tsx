92f692ed1a792eb4f7193532954e032828b9096e
Clickadelic <toby.hopp@gmail.com>
2025-08-01 17:31:58 +0200

    Logo color, Register Login Nav

--- resources/js/Layouts/PublicLayout.tsx
+++ resources/js/Layouts/PublicLayout.tsx
@@ -88,12 +88,20 @@ export default function Public({ children }: PropsWithChildren) {
                                     </Dropdown>
                                 </div>
                             ) : (
-                                <div className="space-x-2">
-                                    <Link href="/login" className="text-gray-500 hover:text-gray-700">
-                                        Login
-                                    </Link>
-                                    <Link href="/register" className="text-gray-500 hover:text-gray-700">
+                                <div className="flex flex-row justify-between items-center space-x-1 sm:space-x-2 md:space-x-3 lg:space-x-4">
+                                    <Link href="/register" className="flex flex-row text-gray-500 hover:text-gray-700">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="mt-1 mr-2 bi bi-check2-circle" viewBox="0 0 16 16">
+                                            <path d="M2.5 8a5.5 5.5 0 0 1 8.25-4.764.5.5 0 0 0 .5-.866A6.5 6.5 0 1 0 14.5 8a.5.5 0 0 0-1 0 5.5 5.5 0 1 1-11 0"/>
+                                            <path d="M15.354 3.354a.5.5 0 0 0-.708-.708L8 9.293 5.354 6.646a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0z"/>
+                                        </svg>
                                         Register
+                                    </Link>
+                                    <Link href="/login" className="flex flex-row text-gray-500 hover:text-gray-700">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="mt-1 mr-2 bi bi-door-open" viewBox="0 0 16 16">
+                                            <path d="M8.5 10c-.276 0-.5-.448-.5-1s.224-1 .5-1 .5.448.5 1-.224 1-.5 1"/>
+                                            <path d="M10.828.122A.5.5 0 0 1 11 .5V1h.5A1.5 1.5 0 0 1 13 2.5V15h1.5a.5.5 0 0 1 0 1h-13a.5.5 0 0 1 0-1H3V1.5a.5.5 0 0 1 .43-.495l7-1a.5.5 0 0 1 .398.117M11.5 2H11v13h1V2.5a.5.5 0 0 0-.5-.5M4 1.934V15h6V1.077z"/>
+                                        </svg>
+                                        Login
                                     </Link>
                                 </div>
                             )}

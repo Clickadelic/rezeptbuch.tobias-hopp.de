69ab37b7d43db9c780b063e71e60d327baa5f9fe
Clickadelic <toby.hopp@gmail.com>
2025-08-01 16:01:38 +0200

    Update deployment script and UI components

--- resources/js/Pages/Welcome.tsx
+++ resources/js/Pages/Welcome.tsx
@@ -1,11 +1,14 @@
-import PublicLayout from '@/Layouts/PublicLayout';
+import PublicLayout from '@/layouts/PublicLayout';
 import { Head } from '@inertiajs/react';
+
+import CustomCarousel from '@/components/CustomCarousel';
 
 export default function Dashboard() {
     return (
         <PublicLayout>
             <Head title="Welcome" />
-            <p>Test für die Webseite. Hallo Welt!</p>
+            <h2 className="text-2xl my-4">Willkommen</h2>
+            <CustomCarousel />
         </PublicLayout>
     );
 }
--- resources/js/components/CustomCarousel.tsx
+++ resources/js/components/CustomCarousel.tsx
@@ -1,5 +1,5 @@
-import { Card, CardContent } from "@/Components/ui/card"
-import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/Components/ui/carousel"
+import { Card, CardContent } from "@/components/ui/card"
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel"
 
 import footSrc from '@images/Hauptgerichte-416x234.jpg';
 
@@ -15,17 +15,15 @@ export default function CustomCarousel() {
             <CarouselContent>
                 {Array.from({ length: 5 }).map((_, index) => (
                 <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
-                    <div>
                     <Card>
-                        <CardContent className="flex flex-col items-center justify-between p-0 border-tl-rounded border-tr-rounded overflow-hidden">
-                            <img src={footSrc} className="w-full border-tl-rounded border-tr-rounded" alt="Food image" />
-                            <div className="w-full flex flex-row justify-start px-2 py-4">
-                                <h2 className="text-2xl font-semibold">Spaghetti Bolognese</h2>
+                        <CardContent className="flex flex-col items-center justify-between p-0">
+                            <img src={footSrc} className="w-full" alt="Food image" />
+                            <div className="w-full flex flex-row justify-start p-4">
+                                <h2 className="text-2xl">Spaghetti Bolognese</h2>
                             </div>
-                            <span className="hidden text-3xl font-semibold">{index + 1}</span>
+                            <span className="hidden text-xl">{index + 1}</span>
                         </CardContent>
                     </Card>
-                    </div>
                 </CarouselItem>
                 ))}
             </CarouselContent>

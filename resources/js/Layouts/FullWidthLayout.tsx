94d11e742484012f059f93412f7d66d0eae97552
Clickadelic <toby.hopp@gmail.com>
2025-08-01 23:51:01 +0200

    Aliasses added, Footer, Header, Images added, Layouts added WIP

--- resources/js/Layouts/FullWidthLayout.tsx
+++ resources/js/Layouts/FullWidthLayout.tsx
@@ -1,19 +1,14 @@
-import AppLogo from '@/Components/AppLogo';
-import { Link } from '@inertiajs/react';
 import { PropsWithChildren } from 'react';
-
-export default function Guest({ children }: PropsWithChildren) {
+import Header from '@/Components/nutshell/Header';
+import Footer from '@/Components/nutshell/Footer';
+export default function FullWidthLayout({ children }: PropsWithChildren) {
     return (
         <div className="min-h-screen flex flex-col justify-between bg-slate-100">
-            <header className="asd">
-                Header
-            </header>
+            <Header />
             <main className="container mx-auto">
                 {children}
             </main>
-            <footer className="asd">
-                Footer
-            </footer>
+            <Footer />
         </div>
     );
 }
--- resources/js/Layouts/SidebarLeftLayout.tsx
+++ resources/js/Layouts/SidebarLeftLayout.tsx
@@ -1,10 +1,8 @@
-import AppLogo from '@/Components/AppLogo';
-import { Link } from '@inertiajs/react';
 import { PropsWithChildren } from 'react';
 
 export default function Guest({ children }: PropsWithChildren) {
     return (
-        <div className="min-h-screen flex flex-col justify-between bg-slate-100 bg-red-400">
+        <div className="min-h-screen flex flex-col justify-between bg-slate-100">
             Sidebar Left Layout
         </div>
     );

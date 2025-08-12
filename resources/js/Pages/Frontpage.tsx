04572848a6bfa20e2627437c78051555ac3aaf1f
Clickadelic <toby.hopp@gmail.com>
2025-08-12 22:14:40 +0200

    Democontent for fonts test added

--- resources/js/Pages/Frontpage.tsx
+++ resources/js/Pages/Frontpage.tsx
@@ -11,23 +11,9 @@ export default function Frontpage({
             <Head title="Willkommen" />
             <FullWidthLayout title="Willkommen">
                 <div className="grid grid-cols-2 gap-4">
-                    <div className="test py-5">
-                        <h1 className="text-oswald text-5xl">Willkommen</h1>
-                        <h2 className="text-oswald text-4xl">Willkommen</h2>
-                        <h3 className="text-oswald text-3xl">Willkommen</h3>
-                        <h4 className="text-oswald text-2xl">Willkommen</h4>
-                        <h5 className="text-oswald text-xl">Willkommen</h5>
-                        <h6 className="text-oswald text-lg">Willkommen</h6>
-                    </div>
-                    <div className="test py-5 space-y-3">
-                        <h1 className="text-oswald text-3xl ">Willkommen</h1>
-                        <h1 className="text-oswald text-3xl">Willkommen</h1>
-                        <h1 className="text-oswald text-3xl">Willkommen</h1>
-                        <h1 className="text-oswald text-3xl">Willkommen</h1>
-                        <h1 className="text-oswald text-3xl">Willkommen</h1>
-                        <h1 className="text-oswald text-3xl">Willkommen</h1>
-                        <h1 className="text-oswald text-3xl">Willkommen</h1>
-                        <h1 className="text-oswald text-3xl">Willkommen</h1>
+                    <div className="bg-slate-200 p-4 rounded-lg">
+                        <h2 className="text-2xl mb-2">Willkommen</h2>
+                        <p className="text-lg">Willkommen auf Toby's Rezeptbuch!</p>
                     </div>
                 </div>
                 

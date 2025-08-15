27dc0269f54cc9b1f2401a7d545d104f1e4ff3da
Clickadelic <toby.hopp@gmail.com>
2025-08-15 11:48:55 +0200

    Build

--- public/build/assets/InputLabel-znwJ0LKf.js
+++
@@ -1,1 +0,0 @@
-import{j as m}from"./app--RWJV6RP.js";function o({value:t,className:e="",children:r,...s}){return m.jsx("label",{...s,className:"block text-sm font-medium text-gray-700 "+e,children:t||r})}export{o as I};

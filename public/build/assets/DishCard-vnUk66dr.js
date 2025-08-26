1e38e3965d9dab966af1fc65a0da1ee23ed1a281
Clickadelic <toby.hopp@gmail.com>
2025-08-26 21:47:32 +0200

    Build

---
+++ public/build/assets/DishCard-vnUk66dr.js
@@ -0,0 +1,1 @@
+import{a as r,j as s}from"./app-D61XmJ9g.js";function e(){return r().props.auth.user,s.jsx("div",{children:"DishCard"})}export{e as default};
---
+++ public/build/assets/index-BtrHBSkB.js
@@ -0,0 +1,1 @@
+import{g as r,c as t}from"./app-D61XmJ9g.js";var a=t();const e=r(a);export{e as R,a as r};

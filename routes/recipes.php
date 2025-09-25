1fccee68aee3276afb6e21d84e46209b769ad742
Clickadelic <toby.hopp@gmail.com>
2025-09-25 03:55:30 +0200

    asdasd

--- routes/recipes.php
+++ routes/recipes.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\RecipeController;
 
 Route::prefix('/rezepte')->group(function () {
     Route::get('/', [RecipeController::class, 'index'])->name('recipes.index');
-    Route::get('/neues-gericht', [RecipeController::class, 'create'])->middleware(['auth', 'verified'])->name('recipes.create');
+    Route::get('/neues-rezept', [RecipeController::class, 'create'])->middleware(['auth', 'verified'])->name('recipes.create');
     Route::post('/', [RecipeController::class, 'store'])->middleware(['auth', 'verified'])->name('recipes.store');
     Route::get('/{recipe}/edit', [RecipeController::class, 'edit'])->middleware(['auth', 'verified'])->name('recipes.edit');
     Route::put('/{recipe}', [RecipeController::class, 'update'])->middleware(['auth', 'verified'])->name('recipes.update');

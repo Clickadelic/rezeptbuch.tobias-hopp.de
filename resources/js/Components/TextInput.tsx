76d50e13acfbf7f21812442c89e79d1308fecefe
Clickadelic <toby.hopp@gmail.com>
2025-09-26 10:30:39 +0200

    textinput styles

--- resources/js/Components/TextInput.tsx
+++ resources/js/Components/TextInput.tsx
@@ -26,7 +26,7 @@ export default forwardRef(function TextInput(
             {...props}
             type={type}
             className={
-                ' bg-gray-50 dark:bg-gray-900 rounded-lg border border-gray-400 text-gray-800 dark:text-gray-200 focus:border-primary focus:ring-primary py-2 px-3 placeholder-gray-200 dark:placeholder:text-gray-600 ' +
+                ' bg-gray-50 dark:bg-gray-900 rounded-lg border border-gray-400 text-gray-800 dark:text-gray-200 focus:border-primary focus:ring-primary py-2 px-3 placeholder:text-gray-600 dark:placeholder:text-gray-600 ' +
                 className
             }
             ref={localRef}

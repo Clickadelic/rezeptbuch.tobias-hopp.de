96630c1b99d2134184d8e64dd1637ed616737771
Clickadelic <toby.hopp@gmail.com>
2025-10-01 00:05:36 +0200

    // @ts-nocheck

--- resources/js/Components/forms/CategoryGrid.tsx
+++ resources/js/Components/forms/CategoryGrid.tsx
@@ -1,6 +1,6 @@
 import { usePage } from "@inertiajs/react";
 import { useEffect, useState } from "react";
-
+// @ts-nocheck
 import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group";
 
 import { PiCookingPot } from "react-icons/pi";

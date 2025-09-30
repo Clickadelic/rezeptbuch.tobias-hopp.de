c017f64acc6142cb5a6cf204e7b94b9e0e25bdea
Clickadelic <toby.hopp@gmail.com>
2025-09-30 03:59:02 +0200

    owner check

--- resources/js/Components/reusables/ContextMenu.tsx
+++ resources/js/Components/reusables/ContextMenu.tsx
@@ -63,7 +63,7 @@ export default function ContextMenu({ recipe }: ContextMenuProps) {
                 <HiOutlineDotsVertical className="size-5" />
             </DropdownMenuTrigger>
             <DropdownMenuContent align="end">
-                    {isOwner(recipe?.user_id) && (
+                    {isOwner(recipe?.user_id ?? 0) && (
                         <>
                             <DropdownMenuItem>
                                 <Link

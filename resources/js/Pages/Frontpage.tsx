5bae7f0fa59dc2adf1bc98911a15dee0742f59f1
Clickadelic <toby.hopp@gmail.com>
2025-09-30 09:08:24 +0200

    Test

--- resources/js/Pages/Frontpage.tsx
+++ resources/js/Pages/Frontpage.tsx
@@ -41,7 +41,7 @@ export default function Frontpage() {
                     <p className="text-base">Viele Grüße und frohes Kochen.</p>
                     <p className="text-2xl font-la-belle-aurore mt-4">Toby</p>
                 </div>
-                <h1>Test</h1>
+                <h1>Tests</h1>
                 {categories.map((category:Category) => {
                     return (
                         <div className="`flex flex-col gap-2" key={category.id}>

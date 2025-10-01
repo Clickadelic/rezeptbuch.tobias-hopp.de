de202d346b46d8471bd7fb6c070ce7378a5a3553
Clickadelic <toby.hopp@gmail.com>
2025-10-01 06:26:04 +0200

    Start of little stuff

--- resources/js/pages/Admin/Index.tsx
+++ resources/js/pages/Admin/Index.tsx
@@ -2,6 +2,7 @@ import { usePage } from '@inertiajs/react';
 import SidebarLeftLayout from '@/layouts/SidebarLeftLayout';
 import AdminSidebar from '@/components/sidebars/AdminSidebar';
 import { Button } from '@/components/ui/button';
+import DataTable from '@/components/reusables/DataTable';
 export default function Index() {
     const { props }: any = usePage();
     const user = props.user;
@@ -14,47 +15,8 @@ export default function Index() {
                 Deine Rollen: <strong>{user.roles?.join(', ')}</strong>
             </p>
             <hr className="my-5 bg-gray-300 dark:bg-gray-700" />
-            <table className="w-full overflow-x-auto">
-                <thead>
-                    <tr>
-                        <th className="p-1 text-left">Id</th>
-                        <th className="p-1 text-left w-48">Username</th>
-                        <th className="p-1 text-left w-64">E-Mail</th>
-                        <th className="p-1 text-left w-32">Rollen</th>
-                        <th className="p-1 text-left">Aktionen</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {users.map((u: any) => (
-                        <tr key={u.id}>
-                            <td className="p-1">{u.id}</td>
-                            <td className="p-1">{u.name}</td>
-                            <td className="p-1">{u.email}</td>
-                            <td className="p-1 flex flex-wrap gap-2">
-                                {u.roles?.map((role: any) => (
-                                    <div
-                                        key={role.id}
-                                        role={role}
-                                        className="bg-primary rounded text-white"
-                                    >
-                                        {role}
-                                    </div>
-                                ))}
-                            </td>
-                            <td className="p-3">
-                                <div className="flex gap-2">
-                                    <Button className="font-medium bg-primary text-white rounded-md">
-                                        Bearbeiten
-                                    </Button>
-                                    <Button className="font-medium bg-rose-500 text-white rounded-md">
-                                        Löschen
-                                    </Button>
-                                </div>
-                            </td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
+            {/* TODO: Add User Management */}
+            <DataTable users={users} />
         </SidebarLeftLayout>
     );
 }

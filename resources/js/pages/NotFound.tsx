b7d7dcacf2c42cf488bd46c9a76f796ced4bb2c7
Clickadelic <toby.hopp@gmail.com>
2025-10-02 17:33:02 +0200

    co9ls

--- resources/js/pages/NotFound.tsx
+++ resources/js/pages/NotFound.tsx
@@ -6,16 +6,18 @@ import { RiHomeLine } from 'react-icons/ri';
 export default function NotFound() {
     return (
         <NoSidebarsLayout title="404 - Seite nicht gefunden">
-            <div className="grid grid-cols-5">
+            <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-5">
                 <div className="col-span-2">
                     <img src={chefkoch} alt="Chef Tobias"/>
                 </div>
-                <div className="col-span-3 flex justify-center items-center">
+                <div className="cols-span-1 sm:col-span-2 md:col-span-3 flex justify-center items-center">
                     <div className="flex flex-col items-start justify-center gap-3">
-                        <h2 className="text-xl text-gray-400">Tadaa, Fehler 404 - Seite nicht gefunden.</h2>
-                        <h2 className="text-3xl italic">Is' mir wohl von der Pfanne gerutscht.</h2>
-                        <p className="text-lg">Die Seite, die du suchst, existiert nicht.</p>
-                        <Link href="/" className="bg-primary rounded px-4 py-2 flex gap-2 text-lg text-white hover:text-gray-600"><RiHomeLine className="mt-[5px]" /> Zur Startseite</Link>
+                        <h2 className="text-xl text-gray-400 leading-4">Tadaa, Fehler 404 - Seite nicht gefunden.</h2>
+                        <h2 className="text-2xl font-la-belle-aurore">..is' mir wohl von der Pfanne gerutscht...</h2>
+                        <p className="text-lg mb-5">Die Seite, die du suchst, existiert nicht.</p>
+                        <Link href="/" className="bg-primary rounded px-3 py-1 text-sm flex gap-2 text-white hover:text-white hover:bg-emerald-700">
+                            <RiHomeLine className="mt-[3px]" /> Zur Startseite
+                        </Link>
                     </div>
                 </div>
             </div>

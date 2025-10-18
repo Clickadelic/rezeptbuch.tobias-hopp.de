81b003cb5b101a23b17d83c87c80d225a7ac025c
Clickadelic <toby.hopp@gmail.com>
2025-10-19 00:45:10 +0200

    Community images

--- resources/js/pages/Community/Index.tsx
+++ resources/js/pages/Community/Index.tsx
@@ -1,20 +1,29 @@
 import FullWidthLayout from '@/layouts/FullWidthLayout';
 
 import { usePage } from '@inertiajs/react';
+import { Link } from '@inertiajs/react';
+import { SharedPageProps } from '@/types';
+
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 /**
  * Displays a list of all users.
  *
  * @return {JSX.Element} The rendered list of users.
  */
 export default function Community() {
-    const { auth } = usePage().props;
-    const { users } = usePage().props;
+    const { auth } = usePage<SharedPageProps>().props;
+    const { users } = usePage<SharedPageProps>().props;
 
     return (
         <FullWidthLayout title="Community">
-            <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-5 gap-5">
-                <li>user</li>
-            </ul>
+            <div className="*:data-[slot=avatar]:ring-background flex -space-x-2 *:data-[slot=avatar]:ring-2 *:data-[slot=avatar]:grayscale">
+                {users?.map((user : any) => (
+                    <Avatar key={user.id}>
+                        <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+                        <AvatarFallback>CN</AvatarFallback>
+                    </Avatar>
+                ))}
+            </div>
         </FullWidthLayout>
     );
 }

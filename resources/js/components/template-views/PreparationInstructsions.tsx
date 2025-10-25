fbf31ffc1500581c7e64ba4ab74558cc95da9a23
Clickadelic <toby.hopp@gmail.com>
2025-10-25 09:22:46 +0200

    preps

--- resources/js/components/template-views/PreparationInstructsions.tsx
+++ resources/js/components/template-views/PreparationInstructsions.tsx
@@ -1,23 +1,33 @@
 import { Recipe } from '@/types/Recipe';
-import Seperator from '@/components/reusables/Seperator';
+import { cn } from '@/lib/utils';
 
 interface PreparationInstructionsProps {
     recipe: Recipe;
+    className?: string
 }
 
-export default function PreparationInstructions({ recipe }: PreparationInstructionsProps) {
+/**
+ * Displays the preparation instructions for a recipe.
+ *
+ * @param {PreparationInstructionsProps} props
+ * @param {Recipe} props.recipe - The recipe to display instructions for.
+ * @param {className} props.className - A class name to apply to the component.
+ * @returns {JSX.Element} - The rendered component.
+ */
+export default function PreparationInstructions({ recipe, className }: PreparationInstructionsProps) {
+    
     if (!recipe.preparation_instructions) return null;
+
     return (
-        <>
-            <div className="flex">
-                <div className="w-full max-w-4xl mx-auto flex flex-col gap-2">
-                    <h4 className="font-medium text-xl">Zubereitung</h4>
-                    <div className="flex flex-col gap-2">
-                        <p>{recipe.preparation_instructions}</p>
-                    </div>
+
+        <div className={cn("flex", className)}>
+            <div className="w-full max-w-4xl mx-auto flex flex-col gap-2">
+                <h4 className="font-medium text-xl">Zubereitung</h4>
+                <div className="flex flex-col gap-2">
+                    <p>{recipe.preparation_instructions}</p>
                 </div>
             </div>
-            <Seperator style="comment-alt" />
-        </>
+        </div>
+
     );
 }

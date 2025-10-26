25773085f952b21ed2c3b7b1f0e93097b6cee6fe
Clickadelic <toby.hopp@gmail.com>
2025-10-26 06:08:02 +0100

    Routes fix...

--- routes/custom/comments.php
+++ routes/custom/comments.php
@@ -8,7 +8,7 @@ use App\Http\Controllers\CommentController;
 Route::prefix('/comments')->group(function () {
     Route::get('/{recipe}', [CommentController::class, 'index'])->name('comments.index');
     Route::delete('/{comment}', [CommentController::class, 'destroy'])->middleware('auth')->name('comments.destroy');
-    
-    Route::put('/comments/{comment}', [CommentController::class, 'update'])->middleware('auth')->name('comments.update');
+    // TODO: Check ob das raus kann
+    // Route::put('/comments/{comment}', [CommentController::class, 'update'])->middleware('auth')->name('comments.update');
     Route::patch('/{comment}', [CommentController::class, 'update'])->middleware('auth')->name('comments.update');
 });

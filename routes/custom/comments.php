12df6bfb19e58849248a7870fc2e1e12d8e3af44
Clickadelic <toby.hopp@gmail.com>
2025-10-26 06:53:26 +0100

    comments put/patch

--- routes/custom/comments.php
+++ routes/custom/comments.php
@@ -6,6 +6,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CommentController;
 
 Route::prefix('/comments')->group(function () {
+    Route::get('/{recipe}', [CommentController::class, 'index'])->name('comments.index');
     Route::delete('/{comment}', [CommentController::class, 'destroy'])->middleware('auth')->name('comments.destroy');
+    
+    Route::put('/comments/{comment}', [CommentController::class, 'update'])->middleware('auth')->name('comments.update');
     Route::patch('/{comment}', [CommentController::class, 'update'])->middleware('auth')->name('comments.update');
 });

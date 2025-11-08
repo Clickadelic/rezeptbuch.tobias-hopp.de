017d274accd6125057ee73bfbd1016fcc379b8d2
Clickadelic <toby.hopp@gmail.com>
2025-11-08 02:17:58 +0100

    card icon

--- resources/js/components/reusables/IconCategorySwitcher.tsx
+++ resources/js/components/reusables/IconCategorySwitcher.tsx
@@ -8,31 +8,33 @@ import { PiCookingPot } from 'react-icons/pi';
 import { RiCake3Line } from 'react-icons/ri';
 import { GiCakeSlice } from 'react-icons/gi';
 import { GiCrystalBars } from 'react-icons/gi';
+import { cn } from '@/lib/utils';
 
 interface CategoryIconSwitcher {
     recipe: Recipe
+    iconColor?: string
 }
 
-export default function CategoryIconSwitcher ({ recipe }: CategoryIconSwitcher) {
-    let categoryIcon = <BiDish className="z-20 size-9 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-gray-400 dark:text-gray-400" />
+export default function CategoryIconSwitcher ({ recipe, iconColor = "text-gray-400" }: CategoryIconSwitcher) {
+    let categoryIcon = <BiDish className={cn("z-20 size-9 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2", iconColor)} />
     switch (recipe?.category?.id) {
         case 1:
-            categoryIcon = <TbSalad className="z-20 size-9 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" />
+            categoryIcon = <TbSalad className={cn("z-20 size-9 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2", iconColor)} />
             break;
         case 2:
-            categoryIcon = <PiCookingPot className="z-20 size-9 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" />
+            categoryIcon = <PiCookingPot className={cn("z-20 size-9 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2", iconColor)} />
             break;
         case 3:
-            categoryIcon = <RiCake3Line className="z-20 size-9 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" />
+            categoryIcon = <RiCake3Line className={cn("z-20 size-9 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2", iconColor)} />
             break;
         case 4:
-            categoryIcon = <LiaCocktailSolid className="z-20 size-9 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" />
+            categoryIcon = <LiaCocktailSolid className={cn("z-20 size-9 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2", iconColor)} />
             break;
         case 5:
-            categoryIcon = <GiCakeSlice className="z-20 size-9 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" />
+            categoryIcon = <GiCakeSlice className={cn("z-20 size-9 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2", iconColor)} />
             break;
         case 6:
-            categoryIcon = <GiCrystalBars className="z-20 size-9 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" />
+            categoryIcon = <GiCrystalBars className={cn("z-20 size-9 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2", iconColor)} />
             break;
     }
     return (

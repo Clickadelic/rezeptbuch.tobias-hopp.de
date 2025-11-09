c5c5add514457d6244a6a07e3779ffd9910a962d
Clickadelic <toby.hopp@gmail.com>
2025-11-09 20:27:32 +0100

    row order

--- resources/js/pages/NotAuthorized.tsx
+++ resources/js/pages/NotAuthorized.tsx
@@ -5,7 +5,7 @@ import missionImpossibleBro from "@images/svg/Mission-Impossible-bro.svg";
 export default function NotAuthorized() {
     return (
         <FullWidthLayout title="Nicht angemeldet?" description="Wenn Du nicht angemeldet bist, bist Du nicht angemeldet.">
-            <div className="flex flex-col sm:flex-row justify-center items-center gap-5">
+            <div className="flex flex-col sm:flex-row-reverse justify-center items-center gap-5">
                 <img src={missionImpossibleBro} className="w-full sm:w-1/2 max-w-96" alt="Chef Tobias" />
                 <div className="w-full flex flex-col justify-center items-center sm:w-1/2">
                     <h2 className="mb-3 text-xl">Du bist nicht eingeloggt und möchtest einen geschützen Bereich besuchen.</h2>

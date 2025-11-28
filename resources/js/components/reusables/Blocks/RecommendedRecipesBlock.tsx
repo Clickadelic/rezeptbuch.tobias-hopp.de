19b021990ec28d261f7d23d57da1f2aaa9b2d65e
Clickadelic <toby.hopp@gmail.com>
2025-11-28 06:36:11 +0100

    csadasdasdasd

--- resources/js/components/reusables/Blocks/RecommendedRecipesBlock.tsx
+++ resources/js/components/reusables/Blocks/RecommendedRecipesBlock.tsx
@@ -5,9 +5,11 @@ import chefBro from '@images/svg/Chef-bro.svg';
 
 import { useMediaQuery } from '@/hooks/useMediaQuery';
 import { Recipe } from '@/types/Recipe';
+import { cn } from '@/lib/utils';
 
 interface RecommendedRecipesBlockProps {
     recipes: Recipe[];
+    className?: string;
 }
 
 /**
@@ -15,10 +17,10 @@ interface RecommendedRecipesBlockProps {
  * The block contains a title, a personal message from the chef, and a carousel of recipe cards.
  * The layout of the block is responsive and changes depending on the screen size.
  */
-export default function RecommendedRecipesBlock({ recipes }: RecommendedRecipesBlockProps) {
+export default function RecommendedRecipesBlock({ recipes, className }: RecommendedRecipesBlockProps) {
     const isDesktop = useMediaQuery('(min-width: 768px)');
     return (
-        <div className="grid grid-cols-1 lg:grid-cols-3">
+        <div className={cn("grid grid-cols-1 lg:grid-cols-3", className)}>
             <div className="col-span-1 flex flex-col gap-2 items-center justify-center">
                 <h2 className="text-2xl">Tagestipps</h2>
                 <h3 className="text-xl text-gray-600 dark:text-gray-400 font-yellowtail">

1bd9cfa4fbb40480ae340f034ba03d5af26e1bcb
Clickadelic <toby.hopp@gmail.com>
2025-11-29 17:32:45 +0100

    UI details

--- resources/js/components/reusables/StarRating.tsx
+++ resources/js/components/reusables/StarRating.tsx
@@ -7,6 +7,9 @@ import { SharedPageProps } from '@/types';
 import { usePermissions } from '@/hooks/usePermissions';
 import { toast } from 'sonner';
 import { LuBookmarkCheck } from "react-icons/lu";
+import { FaHeart, FaSpinner } from 'react-icons/fa6';
+import { FaRankingStar } from "react-icons/fa6";
+import { PiShootingStarLight } from "react-icons/pi";
 interface StarRatingProps {
     recipeId: string | number;
     initialRating?: number;
@@ -103,13 +106,13 @@ export default function StarRating({
                         />
                     </button>
                 ))}
-                {isLoading && (
-                    <span className="text-sm text-gray-600 dark:text-gray-400">
-                        Bewertung wird gesendet...
-                    </span>
-                )}
+                
             </div>
-
+            {isLoading && (
+                <div className="text-sm text-gray-800 dark:text-gray-200">
+                    <p className="flex gap-2"><FaSpinner className="animate-spin size-4 text-primary" />sende Bewertung</p>
+                </div>
+            )}
             {/* Nachricht, wenn User bereits gevotet hat */}
             {community.votes > 0 && (
                 <div className="text-sm text-gray-800 dark:text-gray-200">
@@ -121,7 +124,13 @@ export default function StarRating({
 
             {community.votes > 0 && (
                 <div className="text-sm text-gray-800 dark:text-gray-200">
-                    <p>{community.votes > 0 ? `Bewertungen: ${community.votes}` : ''}</p>
+                    {community.votes === 1 && <p>{`${community.votes} Bewertung`}</p>}
+                    {community.votes > 1 && <p>{`${community.votes} Bewertungen`}</p>}
+                </div>
+            )}
+            {!voted && (
+                <div className="text-sm text-gray-800 dark:text-gray-200">
+                    <p className="flex gap-2"><PiShootingStarLight className="size-5 text-primary" />Gib' Deine Bewertung ab.</p>
                 </div>
             )}
             {voted && (

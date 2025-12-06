f293f82d42ce5473d28024b9c402c9438303727f
Clickadelic <toby.hopp@gmail.com>
2025-12-06 13:44:19 +0100

    wip

--- resources/js/pages/Frontpage.tsx
+++ resources/js/pages/Frontpage.tsx
@@ -6,11 +6,10 @@ import CategorySelectionBlock from '@/components/reusables/Blocks/CategorySelect
 import AuthTeaserBlock from '@/components/reusables/Blocks/AuthTeaserBlock';
 import RecommendedRecipesBlock from '@/components/reusables/Blocks/RecommendedRecipesBlock';
 import RecommendedCocktailsBlock from '@/components/reusables/Blocks/RecommendedCocktailsBlock';
-
+import FaqAccordeon from '@/components/reusables/FaqAccordeon';
 import Seperator from '@/components/reusables/Seperator';
 import StatsBlock from '@/components/reusables/Blocks/StatsBlock';
 
-import FaqAccordeon from '@/components/reusables/FaqAccordeon';
 import { SharedPageProps } from '@/types';
 
 /**
@@ -43,7 +42,7 @@ export default function Frontpage() {
             <Seperator />
             <StatsBlock totalRecipeCount={totalRecipeCount} totalIngredientCount={totalIngredientCount} totalCommentCount={totalCommentCount} className="asd" />
             <Seperator style="question-mark" />
-            <FaqAccordeon isExtendet={false} />
+            <FaqAccordeon />
         </FullWidthLayout>
     );
 }

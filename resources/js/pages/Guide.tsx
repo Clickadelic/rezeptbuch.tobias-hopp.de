d85b01c4f29b4f8c387fe0c268250e017dba23f3
Clickadelic <toby.hopp@gmail.com>
2025-12-06 18:56:57 +0100

    Guide

--- resources/js/pages/Guide.tsx
+++ resources/js/pages/Guide.tsx
@@ -1,12 +1,6 @@
 import SidebarLeftLayout from '@/layouts/SidebarLeftLayout';
 import MainSidebar from '@/components/sidebars/MainSidebar';
-import { Accordion, AccordionContent, AccordionItem, AccordionTrigger, } from '@/components/ui/accordion';
-import { Link } from '@inertiajs/react';
-
-import TitleBlock from '@/components/reusables/Blocks/TitleBlock';
 import GuideAccordeon from '@/components/reusables/GuideAccordeon';
-import { ArrowRight } from 'lucide-react';
-import { Button } from '@/components/ui/button';
 
 /**
  * Leitfaden – Informationen zur Benutzung

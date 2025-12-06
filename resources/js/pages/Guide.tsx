6a787f235bdf81d6e8dbdd34732be302363385cc
Clickadelic <toby.hopp@gmail.com>
2025-12-06 18:15:18 +0100

    Guide WIP

--- resources/js/pages/Guide.tsx
+++ resources/js/pages/Guide.tsx
@@ -3,6 +3,8 @@ import MainSidebar from '@/components/sidebars/MainSidebar';
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger, } from '@/components/ui/accordion';
 import { Link } from '@inertiajs/react';
 
+import TitleBlock from '@/components/reusables/Blocks/TitleBlock';
+import GuideAccordeon from '@/components/reusables/GuideAccordeon';
 import { ArrowRight } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 
@@ -17,47 +19,7 @@ export default function Leitfaden() {
             description="Hinweise und Details zur Verwendung des Rezeptbuchs."
             sidebar={<MainSidebar />}
         >
-            <div className="w-full lg:max-w-xl mx-auto my-4 sm:my-6 md:my-8">
-                <Accordion type="single" collapsible className="mb-8">
-                    <AccordionItem value="item-1">
-                        <AccordionTrigger className="font-medium text-xl">
-                            Allgemeines
-                        </AccordionTrigger>
-                        <AccordionContent>
-                            <p>Im Prinzip möchte ich dem allgemeinen hungrigen Besucher ein wenig Inspiration zum kochen zu bieten. Wie bereits angesprochen, finde ich die bereits bestehenden Kochbucher ziemlich überladen und unübersichtlich, weshalb ich mich dazu entschlossen habe, selbst ein Rezeptbuch zu erstellen.</p>
-                            <p>Die Idee ist einfach: Ein Rezeptbuch, das einfach zu bedienen und einfach zu benutzen ist.</p>
-                        </AccordionContent>
-                    </AccordionItem>
-                    <AccordionItem value="item-2">
-                        <AccordionTrigger className="font-medium text-xl">
-                            Nutzung mit dem Smartphone
-                        </AccordionTrigger>
-                        <AccordionContent>
-                            <p>Die Benutzung der Rezeptbuch-App ist so angelegt, dass Du für Rezepte bequem Bilder bis zu 10 MB mit dem Handy machen kannst. Diese werden beim Upload automatisch direkt verkleinert.</p>
-                            <ul>
-                                <li>Rezepte (und Zutaten) erstellen</li>
-                                <li>Rezepte favorisieren</li>
-                                <li>Jedes Rezept bewerten</li>
-                                <li>Rezepte erstellen</li>
-                            </ul>
-                        </AccordionContent>
-                    </AccordionItem>
-                    <AccordionItem value="item-3">
-                        <AccordionTrigger className="font-medium text-xl">
-                            Rezepte anlegen
-                        </AccordionTrigger>
-                        <AccordionContent>
-                            <p>Um ein neues Rezept anzlegen, klicke nach dem Login einfach auf das Plus-Icon in der rechten unteren Ecke.</p>
-                            <ul>
-                                <li>Rezepte (und Zutaten) erstellen</li>
-                                <li>Rezepte favorisieren</li>
-                                <li>Rezepte erstellen</li>
-                                <li>Rezepte erstellen</li>
-                            </ul>
-                        </AccordionContent>
-                    </AccordionItem>
-                </Accordion>
-            </div>
+            <GuideAccordeon />
         </SidebarLeftLayout>
     );
 }
